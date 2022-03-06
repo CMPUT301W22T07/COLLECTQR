c51974b2729c6b04502d643ce4ae9481e2a24457
oksachi <sachith@ualberta.ca>
2022-03-06 01:48:46 -0700

    Create nav graph, layouts and fragment skeletons
    Navigation graph includes updates to string resources and icons.
    Fragments are associated with a placeholder layout and text.

---
+++ code/app/src/main/java/com/example/collectqr/ui/map/MapViewFragment.java
@@ -0,0 +1,36 @@
+package com.example.collectqr.ui.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.collectqr.R;
+
+public class MapViewFragment extends Fragment {
+
+    private MapViewViewModel mViewModel;
+
+    public static MapViewFragment newInstance() {
+        return new MapViewFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map_view, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MapViewViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
