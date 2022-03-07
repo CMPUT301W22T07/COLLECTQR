5f97f19f90179996a4f2ee504523771e44ce313f
oksachi <sachith@ualberta.ca>
2022-03-06 21:41:01 -0700

    Move from main activity to a different one
    build.gradle updated as well.

--- code/app/src/main/java/com/example/collectqr/MainActivity.java
+++ code/app/src/main/java/com/example/collectqr/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.collectqr;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.collectqr.databinding.ActivityMainBinding;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -26,16 +23,8 @@ public class MainActivity extends AppCompatActivity {
         // Access a Cloud Firestore instance from your Activity
         db = FirebaseFirestore.getInstance();
 
-        /* Bottom Navigation Boilerplate from Android Studio
-           Supplementary source: https://developer.android.com/guide/navigation/navigation-ui#java
-         */
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_leaderboard, R.id.navigation_map, R.id.navigation_history).build();
-        NavController navController =
-                Navigation.findNavController(this, R.id.nav_host_fragment_container_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.bottomnavContainer, navController);
-
+        // Launch main application activity
+        startActivity(new Intent(this, MainAppActivity.class));
     }
 
 }
