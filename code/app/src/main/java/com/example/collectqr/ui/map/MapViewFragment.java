1550343084522680dc08de0c45f7ff8a4ea2de92
oksachi <sachith@ualberta.ca>
2022-03-10 14:58:25 -0700

    Barebones location request and dependency shuffle
    Implemented a basic permission request flow for the MapViewFragment.
    Moved dependencies around to separate the automatically added ones

--- code/app/src/main/java/com/example/collectqr/ui/map/MapViewFragment.java
+++ code/app/src/main/java/com/example/collectqr/ui/map/MapViewFragment.java
@@ -1,23 +1,29 @@
 package com.example.collectqr.ui.map;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.collectqr.ScanQRCodeActivity;
 import com.example.collectqr.databinding.FragmentMapViewBinding;
+import com.google.android.material.snackbar.Snackbar;
 import com.mapbox.maps.MapView;
 import com.mapbox.maps.Style;
 
 public class MapViewFragment extends Fragment {
 
+    final private int LOCATION_REQUEST_CODE = 1;
     // TODO: Test cases for the map bounds
     private MapViewViewModel mViewModel;
     private MapView mMapView;
@@ -42,14 +48,49 @@ public class MapViewFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MapViewViewModel.class);
         // TODO: Use the ViewModel
-        binding.fabLaunchQRScanner.setOnClickListener(view -> {
-            Intent intent = new Intent(this.getActivity(), ScanQRCodeActivity.class);
-            startActivity(intent);
-        });
+        setButtonsActions();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(requireContext(), "Location permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(requireContext(), "Location permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void setButtonsActions() {
+        binding.fabLaunchQRScanner.setOnClickListener(view -> startScanner());
+
+        binding.fabGpsLockLocation.setOnClickListener(view -> setMapToCurrentLocation());
+    }
+
+    private void setMapToCurrentLocation() {
+        /* Permission requesting based off example; uses an older method:
+           https://developer.android.com/training/permissions/requesting */
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(requireContext(), "Location perm granted", Toast.LENGTH_SHORT).show();
+        } else {
+            Snackbar.make(requireView(), "Cannot access location", Snackbar.LENGTH_LONG)
+                    .setAction("Enable Location", view -> requestPermissions(
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            LOCATION_REQUEST_CODE
+                    )).show();
+        }
+    }
+
+    public void startScanner() {
+        Intent intent = new Intent(this.getActivity(), ScanQRCodeActivity.class);
+        startActivity(intent);
     }
 }
