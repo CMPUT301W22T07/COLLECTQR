c74f2835e1ec64267ed13b9d54547d7e869e1196
oksachi <sachith@ualberta.ca>
2022-03-09 21:23:00 -0700

    Merge QR code branch and material 3 branch

--- code/app/src/main/java/com/example/collectqr/ui/map/MapViewFragment.java
+++ code/app/src/main/java/com/example/collectqr/ui/map/MapViewFragment.java
@@ -1,17 +1,23 @@
 package com.example.collectqr.ui.map;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.collectqr.GenerateQRCodeActivity;
+import com.example.collectqr.QRCodeHomeActivity;
 import com.example.collectqr.R;
+import com.example.collectqr.databinding.FragmentMapViewBinding;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.mapbox.maps.MapView;
 import com.mapbox.maps.Style;
 
@@ -22,6 +28,12 @@ public class MapViewFragment extends Fragment {
 
     private MapView mMapView;
 
+    // BRUHHHHHHHHHHHHHHHHHHH
+    // BRUHHHHHHHHHHHHHHHHHHH
+    private FragmentMapViewBinding binding;
+    // BRUHHHHHHHHHHHHHHHHHHH
+    // BRUHHHHHHHHHHHHHHHHHHH
+
     public static MapViewFragment newInstance() {
         return new MapViewFragment();
     }
@@ -29,6 +41,10 @@ public class MapViewFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        binding = FragmentMapViewBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+
         mMapView = new MapView(inflater.getContext());
         mMapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
         return inflater.inflate(R.layout.fragment_map_view, container, false);
@@ -39,6 +55,12 @@ public class MapViewFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MapViewViewModel.class);
         // TODO: Use the ViewModel
+        binding.fabGpsLockLocation.setOnClickListener(view1 -> {
+            Toast toast = Toast.makeText(view1.getContext(), "BRUH", Toast.LENGTH_SHORT);
+            toast.show();
+            Intent intent = new Intent(this.getActivity(), GenerateQRCodeActivity.class);
+            startActivity(intent);
+        });
     }
 
     @Override
