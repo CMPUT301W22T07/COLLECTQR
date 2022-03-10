9e6b90b003b9a5a38a9d5334ffd6a68fbbb3d0c5
oksachi <sachith@ualberta.ca>
2022-03-09 20:23:18 -0700

    Cleanup MainActivity

--- code/app/src/main/java/com/example/collectqr/MainActivity.java
+++ code/app/src/main/java/com/example/collectqr/MainActivity.java
@@ -21,15 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Setup view binding in this activity: see dev docs
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        // Access a Cloud Firestore instance from your Activity
-        db = FirebaseFirestore.getInstance();
-
         // Launch main application activity
-        startActivity(new Intent(this, MainAppActivity.class));
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
