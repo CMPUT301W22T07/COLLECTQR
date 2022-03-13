040542d80f9b6d73f655d5e7ab0358b4f15deba0
Blaise Holloway <blaiseh33@gmail.com>
2022-03-13 00:10:05 -0700

    leaderboard custom list and total scores sorting

---
+++ code/app/src/main/java/com/example/collectqr/ui/leaderboard/CustomList.java
@@ -0,0 +1,59 @@
+package com.example.collectqr.ui.leaderboard;
+
+import static com.google.common.collect.Iterables.size;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.collectqr.R;
+import com.google.firebase.firestore.auth.User;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class CustomList extends ArrayAdapter<User> {
+    private ArrayList<User> users;
+    private Context context;
+
+    public CustomList(Context context, ArrayList<User> games){
+        super(context, 0, games);
+        this.users = users;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        View view = convertView;
+
+        if (view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.content, parent, false);
+        }
+
+        // sort array list based on total points
+        Collections.sort(users, (user1, user2) -> user1.getStats().get(total_points).compareTo(user2.getStats().get(total_points)));
+        // reverse to decreasing order (most points first)
+        Collections.reverse(users);
+
+        User user = users.get(position);
+
+        TextView userName = view.findViewById(R.id.username_text);
+        TextView userScore = view.findViewById(R.id.score_text);
+        TextView userRank = view.findViewById(R.id.rank_text);
+
+        userName.setText(user.getUsername());
+        userScore.setText(user.getStats().get(total_points) + "points");
+        // index of user in sorted list plus 1 = rank
+        userRank.setText("#" + users.indexOf(user)+1);
+
+        return view;
+    }
+}
