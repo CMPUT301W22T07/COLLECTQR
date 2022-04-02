f1f1a3cc1a4f5e0a36208015c423f5f6eaf0f968
Ali Rafiei <a.rafiei1991@gmail.com>
2022-04-01 20:47:13 -0600

    small add

--- code/app/src/main/java/com/example/collectqr/ScanQRCodeLoginActivity.java
+++ code/app/src/main/java/com/example/collectqr/ScanQRCodeLoginActivity.java
@@ -61,6 +61,7 @@ public class ScanQRCodeLoginActivity extends AppCompatActivity {
             @Override
             public void onCodeScanned(String data) {
                 scannedTextView.setText(data);
+                // System.out.println(data);
                 // 'data' is where the string is stored for the QR Code ---------------------------------------------------------------------------------------------------------------
                 // the plan is we login from here, so the code would go here
             }
